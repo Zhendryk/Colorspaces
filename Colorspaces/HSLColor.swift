118480a8eb1c38a292eaffb9146ce95c282881b2
Zhendryk <jonathan.bailey@comcast.net>
2018-12-15 13:15:08 -0500

    First commit

---
+++ Colorspaces/HSLColor.swift
@@ -0,0 +1,9 @@
+//
+//  HSLColor.swift
+//  Colorspaces
+//
+//  Created by Jonathan Bailey on 12/15/18.
+//  Copyright © 2018 Jonathan Bailey. All rights reserved.
+//
+
+import Foundation
---
+++ Colorspaces/RGBColor.swift
@@ -0,0 +1,9 @@
+//
+//  RGBColor.swift
+//  Colorspaces
+//
+//  Created by Jonathan Bailey on 12/15/18.
+//  Copyright © 2018 Jonathan Bailey. All rights reserved.
+//
+
+import Foundation
